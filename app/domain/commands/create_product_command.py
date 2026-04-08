35f209d749d09f22765ee528a2c3e19bff3a9d19
ueno000 <ueno@necknecco.co.jp>
2026-04-08 15:33:04 +0900

    削除

--- app/domain/commands/create_product_command.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class CreateProductCommand(BaseModel):
-    name: str
-    description: Optional[str]
--- app/domain/commands/update_product_command.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class UpdateProductCommand(BaseModel):
-    id: str
-    name: Optional[str]
-    description: Optional[str]
